60b636e0caf8505463c30423fc53e064b3d43e0b
imjae <code@kakao.com>
2022-11-06 19:52:01 +0900

    바이크 컨트롤러, 상태 콘텍스트 클래스 추가

--- Assets/Scripts/Chapter/State/IBikeState.cs
+++ Assets/Scripts/Chapter/State/IBikeState.cs
@@ -1,18 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class IBikeState : MonoBehaviour
+namespace Chapter.State
 {
-    // Start is called before the first frame update
-    void Start()
+    public interface IBikeState
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        void Handle(BikeController controller);
     }
 }

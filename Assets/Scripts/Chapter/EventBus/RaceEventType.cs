b2ba37a9ce4598d15d805a74d0748609e4c45bcb
imjae <code@kakao.com>
2022-11-12 06:44:55 +0900

    레이스 이벤트 타입 열거형 추가

---
+++ Assets/Scripts/Chapter/EventBus/RaceEventType.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chapter.EventBus
+{
+    public enum RaceEventType
+    {
+        COUNTINUE,
+        START,
+        RESTART,
+        PAUSE,
+        STOP,
+        FINISH,
+        QUIT
+    }
+}
